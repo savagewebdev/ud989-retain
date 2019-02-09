fe0eca9546844d168052e8b5311b01bdf320d535
Robin Savage <robin.savage@protonmail.com>
2019-02-08 19:12:59 -0600

    Great Asynchronous MVO
    Added notes for some further clarification. Project creates/draws upon JSON data, parses it, and then passes it to the HTML.

--- js/retain.js
+++ js/retain.js
@@ -1,18 +1,19 @@
-$(function(){
+$(function(){ // Jquery asynchronous processing.
 
     var model = {
         init: function() {
-            if (!localStorage.notes) {
-                localStorage.notes = JSON.stringify([]);
-            }
-        },
-        add: function(obj) {
+            if (!localStorage.notes) { // If localStorage.notes has not loaded,
+                localStorage.notes = JSON.stringify([]); // Then create an array using JSON.stringify.
+            } // JSON.stringify: A method which converts a JavaScript object or value to a JSON string.
+        }, // JSON = JavaScript Object Notation -- a special syntax for JavaScript objects.
+
+        add: function(obj) { // Adds called object notes to JSON file array.
             var data = JSON.parse(localStorage.notes);
             data.push(obj);
             localStorage.notes = JSON.stringify(data);
         },
-        getAllNotes: function() {
-            return JSON.parse(localStorage.notes);
+        getAllNotes: function() { // Returns all notes in JSON file to normal data format via
+            return JSON.parse(localStorage.notes); // JSON.parse.
         }
     };
 
@@ -37,27 +38,27 @@ $(function(){
 
 
     var view = {
-        init: function() {
+        init: function() { // Grab HTML content.
             this.noteList = $('#notes');
             var newNoteForm = $('#new-note-form');
             var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
+            newNoteForm.submit(function(e){ // Event listener for submission.
                 octopus.addNewNote(newNoteContent.val());
                 newNoteContent.val('');
                 e.preventDefault();
             });
-            view.render();
+            view.render(); // Initialization also renders view.render.
         },
         render: function(){
             var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
+            octopus.getNotes().forEach(function(note){ // For each octopus note,
+                htmlStr += '<li class="note">'+ // Add the notes content to a list item.
                         note.content +
                     '</li>';
             });
-            this.noteList.html( htmlStr );
-        }
+            this.noteList.html( htmlStr ); // Change the HTML of the object in question
+        } // to the rendered variable.
     };
 
-    octopus.init();
+    octopus.init(); // Initialize the data.
 });
